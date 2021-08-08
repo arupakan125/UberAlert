c8a5ff95a0c226fe758e940fd8b13c272bc198a0
arupakan125 <43802291+arupakan125@users.noreply.github.com>
2021-08-08 20:06:12 +0900

    Create Order model

--- app/slack/models.py
+++ app/slack/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Order(models.Model):
+    user_id = models.CharField(max_length=16)
+    price = models.IntegerField()
+    ordered_at = models.DateField(default=timezone.now)
