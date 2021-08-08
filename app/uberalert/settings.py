9af67ee993eff67eb96890b4e9a8987d3516bbcd
arupakan125 <43802291+arupakan125@users.noreply.github.com>
2021-08-08 20:20:39 +0900

    Set locale

--- app/uberalert/settings.py
+++ app/uberalert/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-jp'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'JST'
 
 USE_I18N = True
 
