4c04f1a7778d9d112748c9b0f4c891a32d151710
arupakan125 <43802291+arupakan125@users.noreply.github.com>
2021-09-17 12:56:19 +0900

    Add feature to gaman

--- app/slack/models.py
+++ app/slack/models.py
@@ -6,3 +6,8 @@ class Order(models.Model):
     user_id = models.CharField(max_length=16)
     price = models.IntegerField()
     ordered_at = models.DateField(default=timezone.now)
+
+class Gaman(models.Model):
+    user_id = models.CharField(max_length=16)
+    price = models.IntegerField()
+    ordered_at = models.DateField(default=timezone.now)
